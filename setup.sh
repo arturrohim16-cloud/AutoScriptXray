593889f14c92297462089d3d47a6d9d8111b76fa
Ajijainalmaster <arturrohim16@gmail.com>
2026-04-17 00:01:00 +0700

    Replace python installation with python3

--- setup.sh
+++ setup.sh
@@ -104,7 +104,7 @@ sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
 
 echo -e "[ ${BGreen}INFO${NC} ] Preparing the install file"
 apt install git curl -y >/dev/null 2>&1
-apt install python -y >/dev/null 2>&1
+apt install python3 -y >/dev/null 2>&1
 echo -e "[ ${BGreen}INFO${NC} ] Aight good ... installation file is ready"
 sleep 0.5
 echo -ne "[ ${BGreen}INFO${NC} ] Check permission : "
