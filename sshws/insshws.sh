d237c94d35635ced94bd6211ed558ca699417d0f
Ajijainalmaster <arturrohim16@gmail.com>
2026-04-17 13:01:58 +0700

    Update ws-stunnel download URL to new repository

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -4,7 +4,7 @@ cd
 
 #Install Script Websocket-SSH Python
 wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/arturrohim16-cloud/AutoScriptXray/refs/heads/master/sshws/ws-dropbear
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/givpn/AutoScriptXray/master/sshws/ws-stunnel
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/arturrohim16-cloud/AutoScriptXray/refs/heads/master/sshws/ws-stunnel
 
 #izin permision
 chmod +x /usr/local/bin/ws-dropbear
