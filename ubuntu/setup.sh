b13f090765b50f40a3943d3746b7332b260a68fd
Ajijainalmaster <arturrohim16@gmail.com>
2026-04-18 00:23:35 +0700

    Update XRAY installation script URL

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -155,7 +155,7 @@ echo -e "$BGreen          Install XRAY              $NC"
 echo -e "\e[33m-----------------------------------\033[0m"
 sleep 0.5
 clear
-wget https://raw.githubusercontent.com/givpn/AutoScriptXray/master/xray/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
+wget https://raw.githubusercontent.com/arturrohim16-cloud/AutoScriptXray/refs/heads/master/xray/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
 wget https://raw.githubusercontent.com/givpn/AutoScriptXray/master/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 clear
 cat> /root/.profile << END
